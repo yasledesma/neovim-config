df964863692a8d7ae0d13e1d05ac3867743dd9fa
yasledesma <yaslesd@gmail.com>
2023-06-07 01:54:53 -0300

    fix(.gitignore): ignore correct plugin dir

---
+++ after/plugin/telescope.lua
@@ -0,0 +1,22 @@
+local builtin = require('telescope.builtin')
+
+-- default keybinds
+-- <C-c> close telescope
+
+-- finder keybinds
+vim.keymap.set('n', '<leader>ff', builtin.find_files)
+
+-- vim keybinds
+vim.keymap.set('n', '<leader>fb', builtin.buffers)
+
+-- git keybinds (maybe remove these? they don't feel very useful)
+vim.keymap.set('n', '<leader>fgf', builtin.git_files)
+vim.keymap.set('n', '<leader>fgc', builtin.git_commits)
+vim.keymap.set('n', '<leader>fgs', builtin.git_status)
+
+-- lsp keybinds
+vim.keymap.set('n', '<leader>ffr', builtin.lsp_references)
+vim.keymap.set('n', '<leader>ffi', builtin.lsp_implementations)
+vim.keymap.set('n', '<leader>ffd', builtin.lsp_definitions)
+vim.keymap.set('n', '<leader>fft', builtin.lsp_type_definitions)
+
