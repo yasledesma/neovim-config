96e99e91d952c81f5c760f4ada3f0bc9d9f76424
yasledesma <yaslesd@gmail.com>
2023-06-19 01:36:29 -0300

    feat(plugins): add lsp-zero

--- lua/yasmin/plugins.lua
+++ lua/yasmin/plugins.lua
@@ -4,6 +4,27 @@ return require('packer').startup(function(use)
     -- packer can manage itself
     use 'wbthomason/packer.nvim'
 
+    use {
+      'VonHeikemen/lsp-zero.nvim',
+      branch = 'v2.x',
+      requires = {
+        -- LSP Support
+        {'neovim/nvim-lspconfig'},             -- Required
+        {                                      -- Optional
+          'williamboman/mason.nvim',
+          run = function()
+            pcall(vim.cmd, 'MasonUpdate')
+          end,
+        },
+        {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+        -- Autocompletion
+        {'hrsh7th/nvim-cmp'},     -- Required
+        {'hrsh7th/cmp-nvim-lsp'}, -- Required
+        {'L3MON4D3/LuaSnip'},     -- Required
+      }
+    }
+
     -- toggle comments
     use {
         'numToStr/Comment.nvim',
